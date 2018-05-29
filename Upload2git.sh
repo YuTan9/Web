838ee53d307ce638909709f241475362ed40e85a
YuTan9 <s110095@shsh.tw>
2018-05-29 17:01:56 +0800

    up to chap 12

--- Upload2git.sh
+++ Upload2git.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-myvar=$(pwd)
-echo "Your working directory is: ${myvar}, and you are now trying to push the changes to YuTan9's github. What is comment of this commit?"
+mywd=$(pwd)
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "${RED}\tYour working directory is: ${mywd}."
+echo -e "\tYou are now trying to push the changes to YuTan9's github."
+echo -e "\tWhat is comment of this commit?${NC}"
+printf "\t"
 read usrinput
 git add .
 git commit -m "$usrinput"
