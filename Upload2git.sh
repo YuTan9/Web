31e69568b4d4bb25459b10cee00bea4c55d0db39
YuTan9 <s110095@shsh.tw>
2018-05-29 14:31:09 +0800

    :wq

--- Upload2git.sh
+++ Upload2git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 myvar=$(pwd)
-echo "${myvar}"
+echo "Your working directory is: ${myvar}, and you are now trying to push the changes to YuTan9's github. What is comment of this commit?"
 read usrinput
 git add .
 git commit -m "$usrinput"
